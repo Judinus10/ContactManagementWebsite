05ef008d0f9a36c40af59221645c449b506be8d0
judinus10 <judes.20222403@iit.ac.lk>
2025-04-28 09:26:18 +0530

    Connect Form to Save Contact

--- demo/src/main/java/com/example/controller/HomeController.java
+++ demo/src/main/java/com/example/controller/HomeController.java
@@ -1,13 +1,32 @@
 package com.example.controller;
 
+import com.example.model.Contact;
+import com.example.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
     @GetMapping("/")
-    public String home() {
-        return "index"; // refers to templates/index.html
+    public String home(Model model) {
+        List<Contact> contacts = contactRepository.findAll();
+        model.addAttribute("contacts", contacts);
+        return "index";
+    }
+
+    @PostMapping("/addContact")
+    public String addContact(@ModelAttribute Contact contact) {
+        contactRepository.save(contact); 
+        return "redirect:/";
     }
 }
-
